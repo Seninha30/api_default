821bb5f2008099a8d40d137ef79fb1958b284576
Ricardo Araujo <ricardoaraujosena.sena@gmail.com>
2019-12-05 23:41:19 -0300

    implementação do mapeamento das entidades

--- Default.Infra/Persistence/EF/DefaultContext.cs
+++ Default.Infra/Persistence/EF/DefaultContext.cs
@@ -1,5 +1,8 @@
 ﻿using Default.Domain.Entity;
+using Default.Domain.ObjectValues;
+using Default.Infra.Persistence.MAP;
 using Microsoft.EntityFrameworkCore;
+using prmToolkit.NotificationPattern;
 
 namespace Default.Infra.Persistence.EF
 {
@@ -9,8 +12,22 @@ namespace Default.Infra.Persistence.EF
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //todo ajustar a connection string na classe startup
             optionsBuilder.UseSqlServer("connctionString");
             base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //ignorar classes
+            modelBuilder.Ignore<Notification>();
+            modelBuilder.Ignore<Nome>();
+            modelBuilder.Ignore<Email>();
+
+            modelBuilder.ApplyConfiguration(new UserMap());
+
         }
     }
 }
---
+++ Default.Infra/Persistence/MAP/UserMap.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Default.Domain.Entity;
+using Default.Domain.ObjectValues;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Default.Infra.Persistence.MAP
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Ativo).HasDefaultValue(true);
+            builder.Property(x => x.DataCriacao);
+            builder.Property(x => x.DataAlteracao).IsRequired();
+            builder.Property(x => x.PassWord).HasMaxLength(36).IsRequired();
+            builder.OwnsOne<Nome>(x => x.Nome, cb => {
+                cb.Property(x => x.PrimeiroNome).HasMaxLength(50).HasColumnName("PrimeiroNome").IsRequired();
+                cb.Property(x => x.UltimoNome).HasMaxLength(50).HasColumnName("Ultimonome").IsRequired();
+            });
+            builder.OwnsOne<Email>(x => x.Email, cb =>
+            {
+                cb.Property(x => x.Endereco).HasColumnName("EnderecoEmail").IsRequired();
+            });
+        }        
+    }
+}
